9588ddd63965600aafe13828cf1ee2c3ec8f4caa
kciter <kciter@naver.com>
2021-12-16 19:00:36 +0900

    working...

--- scripts/build.ts
+++ scripts/build.ts
@@ -7,7 +7,7 @@ import { buildAllPackages } from './utils/build-all-packages';
 const { argv }: { argv: any } = yargs(hideBin(process.argv))
   .option('all', {
     type: 'boolean',
-    default: true,
+    default: false,
     description: 'Build all packages.',
   })
   .option('project', {
