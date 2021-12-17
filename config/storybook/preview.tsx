d0e17062b36524b291fc31fae0ec5d6357512a2b
kciter <kciter@naver.com>
2021-12-17 17:18:50 +0900

    Storybook set default to light mode

--- config/storybook/preview.tsx
+++ config/storybook/preview.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useDarkMode } from 'storybook-dark-mode';
 import { CoProvider, ColorSchemeProvider } from '@co-design/core';
 
-export const parameters = { layout: 'fullscreen' };
+export const parameters = { layout: 'fullscreen', darkMode: { current: 'light' } };
 
 function ThemeWrapper(props: any) {
   return (
