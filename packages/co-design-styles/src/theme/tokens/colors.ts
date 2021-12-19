d33dfaefad47471689e3a9c071d2d2db57b49971
Lee Sun-Hyoup <kciter@naver.com>
2021-12-20 03:42:35 +0900

    Change default lightBackground color

--- packages/co-design-styles/src/theme/tokens/colors.ts
+++ packages/co-design-styles/src/theme/tokens/colors.ts
@@ -4,7 +4,7 @@ export const DEFAULT_COLORS = {
   transparent: 'transparent',
   white: '#FFF',
   black: '#000',
-  lightBackground: '#FFF',
+  lightBackground: '#EFEFEF',
   lightBorder: '#E9EBF0',
   darkBackground: '#212121',
   darkBorder: '#424242',
