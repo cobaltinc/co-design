28ddec7904e1889b11d76e70da365c0340522f74
Lee Sun-Hyoup <kciter@naver.com>
2022-01-02 02:52:34 +0900

    Remove IconButtonVariant type

--- packages/co-design-core/src/components/IconButton/index.ts
+++ packages/co-design-core/src/components/IconButton/index.ts
@@ -1,6 +1,5 @@
 export { IconButton } from './IconButton';
 export type { IconButtonProps } from './IconButton';
-export type { IconButtonVariant } from './IconButton.style';
 
 export { CloseButton } from './CloseButton';
 export type { CloseButtonProps } from './CloseButton';
