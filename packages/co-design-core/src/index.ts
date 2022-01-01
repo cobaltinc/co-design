10991985677f9c1cb6f94d06b3764a39c66f7a52
Lee Sun-Hyoup <kciter@naver.com>
2022-01-02 02:49:28 +0900

    Add export createStyles

--- packages/co-design-core/src/index.ts
+++ packages/co-design-core/src/index.ts
@@ -1,3 +1,3 @@
-export { CoProvider, useCoTheme, ColorSchemeProvider, useCoColorScheme, Global } from '@co-design/styles';
+export { createStyles, CoProvider, useCoTheme, ColorSchemeProvider, useCoColorScheme, Global } from '@co-design/styles';
 export type { CoTheme, Co, CoComponentProps } from '@co-design/styles';
 export * from './components';
