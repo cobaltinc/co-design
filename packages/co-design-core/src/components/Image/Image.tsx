e1d3259f3430c53f9af952ebc78a1e01960a47f9
kciter <kciter@naver.com>
2022-03-14 18:55:50 +0900

    Image 컴포넌트에 Prop 설명 주석을 추가했다.

--- packages/co-design-core/src/components/Image/Image.tsx
+++ packages/co-design-core/src/components/Image/Image.tsx
@@ -18,14 +18,31 @@ const onIntersection: IntersectionObserverCallback = (entries, io) => {
 export type ImageStylesNames = ClassNames<typeof useStyles>;
 
 export interface ImageProps extends CoComponentProps<ImageStylesNames>, Omit<React.ComponentPropsWithoutRef<'h1'>, 'placeholder'> {
+  /** true일 경우 lazy load가 적용됩니다. */
   lazy?: boolean;
+
+  /** lazy 속성이 true이고 Image 컴포넌트 크기 * threshold만큼 화면에 보일 경우 이미지가 로드됩니다. */
   threshold?: number;
+
+  /** 이미지 src를 정합니다. */
   src: string;
+
+  /** 이미지가 아직 로드되지 않은 경우 보여줄 요소를 정합니다. */
   placeholder?: React.ReactNode;
+
+  /** Image 컴포넌트의 radius를 정합니다. */
   radius?: CoRadius | number;
+
+  /** Image 컴포넌트의 너비를 정합니다. */
   width?: number | string;
+
+  /** Image 컴포넌트의 높이를 정합니다. */
   height?: number | string;
+
+  /** 이미지 설명을 추가합니다. */
   alt?: string;
+
+  /** Image 컴포넌트와 이미지 컨텐츠의 크기를 어떻게 맞출 것인지 정합니다. */
   fit?: 'cover' | 'fill' | 'contain';
 }
 
