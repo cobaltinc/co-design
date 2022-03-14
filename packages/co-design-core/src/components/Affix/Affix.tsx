a4d0fbbc83e62c855df4bd839469ac850c68ce94
kciter <kciter@naver.com>
2022-03-14 14:45:29 +0900

    Affix 컴포넌트의 Prop 설명 주석을 추가했다.

--- packages/co-design-core/src/components/Affix/Affix.tsx
+++ packages/co-design-core/src/components/Affix/Affix.tsx
@@ -5,8 +5,13 @@ import { Portal } from '../Portal/Portal';
 import { getFieldValue } from '../../utils';
 
 export interface AffixProps extends CoComponentProps, React.ComponentPropsWithoutRef<'div'> {
+  /** Affix 컴포넌트가 마운트될 DOM을 지정합니다. */
   target?: HTMLDivElement;
+
+  /** Affix 컴포넌트의 z-index를 정합니다. */
   zIndex?: CoZIndex | number;
+
+  /** Affix 컴포넌트의 위치를 정합니다. */
   position?: {
     top?: string | number;
     left?: string | number;
