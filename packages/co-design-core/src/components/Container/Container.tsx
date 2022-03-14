805a69d30d9e3f5c3c4d0a899fc44a10a19da74d
kciter <kciter@naver.com>
2022-03-14 16:22:40 +0900

    Container 컴포넌트의 Prop 설명 주석을 추가했다.

--- packages/co-design-core/src/components/Container/Container.tsx
+++ packages/co-design-core/src/components/Container/Container.tsx
@@ -6,9 +6,16 @@ import { View } from '../View';
 export type ContainerStylesNames = ClassNames<typeof useStyles>;
 
 export interface ContainerProps extends CoComponentProps<ContainerStylesNames>, React.ComponentPropsWithoutRef<'div'> {
+  /** Container 컴포넌트의 너비를 정합니다. */
   size?: CoBreakpoints | number;
+
+  /** Container 컴포넌트 내용에 padding을 줍니다. */
   padding?: CoSpacing | number;
+
+  /** Container 컴포넌트의 너비를 상위 요소의 너비에 맞춥니다. */
   fluid?: boolean;
+
+  /** Container 컴포넌트의 크기가 breakpoint 단위로 줄어듭니다. */
   break?: boolean;
 }
 
