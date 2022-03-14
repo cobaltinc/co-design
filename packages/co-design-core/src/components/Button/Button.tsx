3f01f8b0e04162d815c231b5d74c1d86eac24427
kciter <kciter@naver.com>
2022-03-14 15:50:39 +0900

    Button 컴포넌트의 Prop 설명 주석을 추가했다.

--- packages/co-design-core/src/components/Button/Button.tsx
+++ packages/co-design-core/src/components/Button/Button.tsx
@@ -8,14 +8,31 @@ import { CO_HEIGHTS } from '../../constants';
 export type ButtonStylesNames = ClassNames<typeof useStyles>;
 
 export interface SharedButtonProps extends CoComponentProps<ButtonStylesNames> {
+  /** Button 컴포넌트의 크기를 정합니다. */
   size?: CoSize;
+
+  /** Button 컴포넌트의 색상을 정합니다. */
   color?: CoPalette;
+
+  /** Button 컴포넌트의 모양을 지정합니다. */
   variant?: ButtonVariant;
+
+  /** true일 경우 좌우 공간을 모두 차지합니다. */
   fullWidth?: boolean;
+
+  /** button 요소의 type을 지정합니다. */
   type?: 'submit' | 'button' | 'reset';
+
+  /** true일 경우 button이 disabled 됩니다. */
   disabled?: boolean;
+
+  /** Button 컴포넌트 좌측 영역에 요소를 추가합니다. */
   leftIcon?: React.ReactNode;
+
+  /** Button 컴포넌트 좌측 영역에 요소를 추가합니다. */
   rightIcon?: React.ReactNode;
+
+  /** true일 경우 로딩 상태가 됩니다. */
   loading?: boolean;
 }
 
