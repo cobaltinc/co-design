c9036aab4e987c01c1a09e6d118b87c9b1b04993
kciter <kciter@naver.com>
2022-03-15 15:40:56 +0900

    Tag 컴포넌트에 Prop 설명 주석을 남겼다.

--- packages/co-design-core/src/components/Tag/Tag.tsx
+++ packages/co-design-core/src/components/Tag/Tag.tsx
@@ -6,8 +6,13 @@ import { View } from '../View';
 export type TagStylesNames = ClassNames<typeof useStyles>;
 
 export interface TagProps extends CoComponentProps<TagStylesNames>, React.ComponentPropsWithoutRef<'div'> {
+  /** Tag 컴포넌트의 색상을 정합니다. */
   color: CoPalette;
+
+  /** Tag 컴포넌트의 크기를 상위 컴포넌트의 너비에 맞춥니다. */
   fullWidth?: boolean;
+
+  /** Tag 컴포넌트에 포함된 문자열에 bold를 적용합니다. */
   strong?: boolean;
 }
 
