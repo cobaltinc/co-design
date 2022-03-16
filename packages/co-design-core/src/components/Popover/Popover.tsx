293333295c3e52f26f512fe414b4a0bb739ed2ca
kciter <kciter@naver.com>
2022-03-16 11:43:37 +0900

    Popover 컴포넌트에 Prop 설명 주석을 남겼다.

--- packages/co-design-core/src/components/Popover/Popover.tsx
+++ packages/co-design-core/src/components/Popover/Popover.tsx
@@ -10,18 +10,46 @@ import { CoTransition, Transition } from '../Transition';
 export type PopoverStylesNames = ClassNames<typeof useStyles>;
 
 export interface PopoverProps extends CoComponentProps<PopoverStylesNames>, React.ComponentPropsWithoutRef<'div'> {
+  /**
+   * true일 경우 Popover 컴포넌트를 보여줍니다.
+   * Popover 컴포넌트를 직접 제어할 경우 사용하는 속성입니다.
+   */
   visible?: boolean;
+
+  /** Popover 컴포넌트에 포함시킬 요소를 넣습니다. */
   content: React.ReactNode;
+
+  /** Popover 컴포넌트 컨텐츠 영역에 padding을 적용합니다. */
   contentPadding?: CoSpacing | number;
+
+  /** Popover 컴포넌트에 화살표를 보여줄지 정합니다. */
   withArrow?: boolean;
+
+  /** Popover 컴포넌트의 너비를 지정한 값으로 고정합니다. */
   width?: number;
+
+  /** Popover 컴포넌트가 보여질 위치를 정합니다. */
   placement?: PopoverPlacement;
+
+  /** Popover 컴포넌트를 등장시킬 이벤트를 정합니다. */
   trigger?: PopoverTrigger;
+
+  /** Popover 컴포넌트의 z-index를 정합니다. */
   zIndex?: CoZIndex | number;
+
+  /** Popover 컴포넌트가 마운트될 요소를 정합니다. */
   target?: HTMLDivElement;
+
+  /** Popover 컴포넌트가 등장할 때 실행될 트랜지션을 정합니다. */
   transition?: CoTransition;
+
+  /** 트랜지션이 실행되는 시간을 ms단위로 정합니다. */
   transitionDuration?: number;
+
+  /** 트랜지션의 타이밍 함수를 정합니다. */
   transitionTimingFunction?: string;
+
+  /** visible이 변경될 경우 실행됩니다. */
   onChangeVisible?(visible: boolean): void;
 }
 
