1471cf24c81ed4be85d195c1827a7f8342254ca6
Lee Sun-Hyoup <kciter@naver.com>
2022-04-11 04:00:59 +0900

    필요없는 코드를 정리했다.

--- packages/co-design-core/src/components/PanelStack/ArrowLeft.tsx
+++ packages/co-design-core/src/components/PanelStack/ArrowLeft.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
-import { SVGProps } from 'react';
 
-export const ArrowLeft = (props: SVGProps<SVGSVGElement>) => (
-  <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+export const ArrowLeft = () => (
+  <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path fillRule="evenodd" clipRule="evenodd" d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2Z" fill="currentColor" />
   </svg>
 );
