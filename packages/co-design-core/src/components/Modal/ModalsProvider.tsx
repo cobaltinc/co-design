429aaadb39f245cb22c232f2c1c95fa7e78447a0
kciter <kciter@naver.com>
2022-05-16 16:00:28 +0900

    모달 상태를 reducer로 수정하고 closeTop 함수를 추가했다.

--- packages/co-design-core/src/components/Modal/ModalsProvider.tsx
+++ packages/co-design-core/src/components/Modal/ModalsProvider.tsx
@@ -1,14 +1,9 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { Modal } from './Modal';
-import { useList } from '@co-design/hooks';
 import { randomId } from './utils';
-import { ModalsContext, ModalsContextProps, ModalSettings, ConfirmLabels, OpenConfirmModal, ModalState } from './context';
+import { ModalsContext, ModalsContextProps, ModalSettings, ConfirmLabels, OpenConfirmModal, OpenContextModal, ContextModalProps } from './context';
 import { ConfirmModal } from './ConfirmModal';
-
-export interface ContextModalProps {
-  context: ModalsContextProps;
-  id: string;
-}
+import { modalsReducer } from './reducer';
 
 export interface ModalsProviderProps {
   children: React.ReactNode;
@@ -17,92 +12,166 @@ export interface ModalsProviderProps {
   labels?: ConfirmLabels;
 }
 
-function extractConfirmModalProps(props: OpenConfirmModal) {
+function separateConfirmModalProps(props: OpenConfirmModal) {
   if (!props) {
     return { confirmProps: {}, modalProps: {} };
   }
 
   const { id, children, onCancel, onConfirm, closeOnConfirm, closeOnCancel, cancelProps, confirmProps, groupProps, labels, ...others } = props;
 
-  return { id, ...others };
+  return {
+    confirmProps: {
+      id,
+      children,
+      onCancel,
+      onConfirm,
+      closeOnConfirm,
+      closeOnCancel,
+      cancelProps,
+      confirmProps,
+      groupProps,
+      labels,
+    },
+    modalProps: {
+      id,
+      ...others,
+    },
+  };
 }
 
 export function ModalsProvider({ children, modalProps, labels, modals }: ModalsProviderProps) {
-  const [state, handlers] = useList<ModalState>([]);
-  const [currentModal, setCurrentModal] = useState<ModalState>({
-    id: null,
-    props: null,
-    type: 'content',
-  });
-  const closeAll = () => handlers.setState([]);
+  const [state, dispatch] = useReducer(modalsReducer, { modals: [], current: null });
+
+  const closeAll = (canceled?: boolean) => {
+    state.modals.forEach((modal) => {
+      if (modal.type === 'confirm' && canceled) {
+        modal.props?.onCancel?.();
+      }
+
+      modal.props?.onClose?.();
+    });
+    dispatch({ type: 'CLOSE_ALL' });
+  };
 
   const openModal = (props: ModalSettings) => {
     const id = props.id || randomId();
-    handlers.append({ id, props, type: 'content' });
-    setCurrentModal({ id, props, type: 'content' });
+    dispatch({
+      type: 'OPEN',
+      payload: {
+        id,
+        type: 'content',
+        props,
+      },
+    });
     return id;
   };
 
   const openConfirmModal = (props: OpenConfirmModal) => {
     const id = props.id || randomId();
-    handlers.append({ id, props, type: 'confirm' });
-    setCurrentModal({ id, props, type: 'confirm' });
+    dispatch({
+      type: 'OPEN',
+      payload: {
+        id,
+        type: 'confirm',
+        props,
+      },
+    });
     return id;
   };
 
-  const openContextModal = (modal: string, props: OpenConfirmModal) => {
+  const openContextModal = (modal: string, props: OpenContextModal) => {
     const id = props.id || randomId();
-    handlers.append({ id, props, type: 'context', ctx: modal });
-    setCurrentModal({ id, props, type: 'context', ctx: modal });
+    dispatch({
+      type: 'OPEN',
+      payload: {
+        id,
+        type: 'context',
+        props,
+        ctx: modal,
+      },
+    });
     return id;
   };
 
-  const closeModal = (id: string) => {
-    const index = state.findIndex((item) => item.id === id);
-    if (state.length <= 1) {
-      closeAll();
-    } else {
-      const modal = state.find((item) => item.id === id);
-      modal?.props?.onClose?.();
-      index !== -1 && handlers.remove(index);
-      setCurrentModal(
-        state[state.length - 2] || {
-          id: null,
-          props: null,
-          type: 'content',
-        },
-      );
+  const closeModal = (id: string, canceled?: boolean) => {
+    if (state.modals.length <= 1) {
+      closeAll(canceled);
+      return;
     }
+
+    const modal = state.modals.find((item) => item.id === id);
+    if (modal?.type === 'confirm' && canceled) {
+      modal.props?.onCancel?.();
+    }
+    modal?.props?.onClose?.();
+    dispatch({ type: 'CLOSE', payload: modal.id });
   };
 
-  const ContextModal = currentModal?.type === 'context' ? modals[currentModal?.ctx] : () => null;
+  const closeTop = (canceled?: boolean) => {
+    if (state.modals.length <= 1) {
+      closeAll(canceled);
+      return;
+    }
 
-  const ctx = {
-    modals: state,
+    const modal = state.modals[state.modals.length - 1];
+    if (modal?.type === 'confirm' && canceled) {
+      modal.props?.onCancel?.();
+    }
+    modal?.props?.onClose?.();
+    dispatch({ type: 'CLOSE', payload: modal.id });
+  };
+
+  const ctx: ModalsContextProps = {
+    modals: state.modals,
     openModal,
     openConfirmModal,
     openContextModal,
     closeModal,
     closeAll,
+    closeTop,
   };
 
-  const content =
-    currentModal?.type === 'context' ? (
-      <ContextModal context={ctx} id={currentModal?.id} />
-    ) : currentModal?.type === 'confirm' ? (
-      <ConfirmModal {...currentModal.props} id={currentModal.id} labels={currentModal.props.labels || labels} />
-    ) : (
-      currentModal?.props?.children
-    );
+  const getCurrentModal = () => {
+    switch (state.current?.type) {
+      case 'context': {
+        const { innerProps, ...rest } = state.current.props;
+        const ContextModal = modals[state.current.ctx];
+
+        return {
+          modalProps: rest,
+          content: <ContextModal innerProps={innerProps} context={ctx} id={state.current.id} />,
+        };
+      }
+      case 'confirm': {
+        const { modalProps: separatedModalProps, confirmProps: separatedConfirmProps } = separateConfirmModalProps(state.current.props);
+
+        return {
+          modalProps: separatedModalProps,
+          content: <ConfirmModal {...separatedConfirmProps} id={state.current.id} labels={state.current.props.labels || labels} />,
+        };
+      }
+      case 'content': {
+        const { children: currentModalChildren, ...rest } = state.current.props;
+
+        return {
+          modalProps: rest,
+          content: <>{currentModalChildren}</>,
+        };
+      }
+      default: {
+        return {
+          modalProps: {},
+          content: null,
+        };
+      }
+    }
+  };
+
+  const { modalProps: currentModalProps, content } = getCurrentModal();
 
   return (
     <ModalsContext.Provider value={ctx}>
-      <Modal
-        {...modalProps}
-        {...(currentModal?.type === 'confirm' ? extractConfirmModalProps(currentModal?.props) : currentModal?.props)}
-        opened={state.length > 0}
-        onClose={() => closeModal(currentModal?.id)}
-      >
+      <Modal {...modalProps} {...currentModalProps} opened={state.modals.length > 0} onClose={() => closeModal(state.current.id)}>
         {content}
       </Modal>
 
