600d00dca51517b481632e9bb56d2692f277a51a
Lee Sun-Hyoup <kciter@naver.com>
2022-09-14 18:06:02 +0900

    Fixed wrong icon names

--- packages/co-design-icons/src/ArrowDown.tsx
+++ packages/co-design-icons/src/ArrowDown.tsx
@@ -3,7 +3,7 @@ import { SVGProps } from 'react';
 
 const SvgArrowDown = (props: SVGProps<SVGSVGElement>) => (
   <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <path fillRule="evenodd" clipRule="evenodd" d="M13 20V7.83l5.59 5.59L20 12l-8-8-8 8 1.41 1.41L11 7.83V20h2Z" fill="currentColor" />
+    <path fillRule="evenodd" clipRule="evenodd" d="M11 4v12.17l-5.59-5.59L4 12l8 8 8-8-1.41-1.41L13 16.17V4h-2Z" fill="currentColor" />
   </svg>
 );
 
