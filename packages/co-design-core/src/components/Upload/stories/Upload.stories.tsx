35ae3e38711cf25b78014d7fe374db859c6b49a3
StellaKim1230 <stellakim1230@gmail.com>
2023-02-24 18:27:58 +0900

    Upload 컴포넌트에 multiple 속성을 추가했다.

--- packages/co-design-core/src/components/Upload/stories/Upload.stories.tsx
+++ packages/co-design-core/src/components/Upload/stories/Upload.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Upload } from '../Upload';
 import { Button } from '../../Button';
 
@@ -20,7 +20,7 @@ export const Default = () => {
 export const AccessFile = () => {
   return (
     <div style={{ padding: 30 }}>
-      <Upload>{(file) => <Button>{file ? file.name : 'Click me'}</Button>}</Upload>
+      <Upload>{(file: File) => <Button>{file ? file.name : 'Click me'}</Button>}</Upload>
     </div>
   );
 };
@@ -29,12 +29,36 @@ export const Draggable = () => {
   return (
     <div style={{ padding: 30 }}>
       <Upload draggable>
-        {(file, dragging) => (
+        {(file: File, dragging) => (
           <div style={{ height: 100, width: 300, border: '4px dashed #aaa', borderColor: dragging ? 'black' : '#aaa' }}>
             {file ? file.name : 'Click or drag file to this area to upload.'}
           </div>
         )}
       </Upload>
+    </div>
+  );
+};
+
+export const Multiple = () => {
+  const [files, setFiles] = useState<File[]>();
+
+  return (
+    <div style={{ padding: 30 }}>
+      <Upload multiple onChange={async (files: File[]) => setFiles(files)}>
+        {(files: File[], dragging) => (
+          <div style={{ height: 100, width: 300, border: '4px dashed #aaa', borderColor: dragging ? 'black' : '#aaa' }}>
+            Click or drag file to this area to upload.
+          </div>
+        )}
+      </Upload>
+
+      <div style={{ marginTop: 40 }}>
+        {files?.map((file, index) => (
+          <div key={index} style={{ padding: 16, marginBottom: 8, width: 300, border: '1px solid #aaa' }}>
+            file: {file.name}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
