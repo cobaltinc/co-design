0ea2b4d58b60a2e0b26b5312465b9c7023924508
healtheloper <58503584+healtheloper@users.noreply.github.com>
2023-08-09 13:34:12 +0900

    Storybook v7 으로 마이그레이션했다

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,18 @@
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended'
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
+    project: './tsconfig.json'
   },
-  plugins: [
-    "react-refresh"
-  ],
+  plugins: ["react-refresh"],
   rules: {
     'prefer-const': 'off',
     "no-unsafe-optional-chaining": "off",
-    
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/ban-types': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
-    
     "react-hooks/exhaustive-deps": "off",
-
-    "react-refresh/only-export-components": "off",
+    "react-refresh/only-export-components": "off"
   }
 };
