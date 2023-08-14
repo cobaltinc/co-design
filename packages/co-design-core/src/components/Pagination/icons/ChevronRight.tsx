91162049ed3587d6af0ef9bb7ff40ea205aff7cf
healtheloper <58503584+healtheloper@users.noreply.github.com>
2023-08-14 18:36:06 +0900

    Pagination 의 아이콘을 추가했다

---
+++ packages/co-design-core/src/components/Pagination/icons/ChevronRight.tsx
@@ -0,0 +1,14 @@
+import React, { SVGProps } from 'react';
+
+const ChevronRight = (props: SVGProps<SVGSVGElement>) => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M8.58984 16.59L13.1698 12L8.58984 7.41L9.99984 6L15.9998 12L9.99984 18L8.58984 16.59Z"
+      fill="#5D636D"
+    />
+  </svg>
+);
+
+export default ChevronRight;
